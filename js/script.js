79d9b776b3d0b7ee9c425d44b1fa8840d0e95f6e
Lucas Pinto <lucasdamianpinto1987@gmail.com>
2024-06-18 01:26:19 +0200

    modifique el codigo script porque tenia una funcion que me era imposible entenderla y encontre una manera mas sencilla

--- js/script.js
+++ js/script.js
@@ -2,22 +2,22 @@ import peliculas from "./peliculas.js";
 
 const baseImageUrl = "https://image.tmdb.org/t/p/w500";
 
-// Contenedores de los géneros
+//  Contenedores de los géneros
 const generoAccion = document.getElementById("genero-28");
 const generoThriller = document.getElementById("genero-53");
 const generoAventura = document.getElementById("genero-12");
 
-// Función para crear un elemento de película
 function crearElementoPelicula(pelicula) {
   const peliculaDiv = document.createElement("div");
   peliculaDiv.classList.add("pelicula");
 
   const img = document.createElement("img");
-  img.src = `${baseImageUrl}${pelicula.poster_path}`;
+  img.src = baseImageUrl + pelicula.poster_path; // Uso de concatenación de cadenas
   img.alt = pelicula.title;
 
   const titulo = document.createElement("h3");
   titulo.textContent = pelicula.title;
+  titulo.setAttribute("title", pelicula.title); // Añadir el atributo title
 
   peliculaDiv.appendChild(img);
   peliculaDiv.appendChild(titulo);
@@ -25,7 +25,7 @@ function crearElementoPelicula(pelicula) {
   return peliculaDiv;
 }
 
-// Función para agregar películas a un contenedor específico
+// Creo esta función para agregar películas al contenedor
 function agregarPeliculasAContenedor(peliculas, contenedor) {
   peliculas.forEach((pelicula) => {
     const elementoPelicula = crearElementoPelicula(pelicula);
@@ -33,7 +33,7 @@ function agregarPeliculasAContenedor(peliculas, contenedor) {
   });
 }
 
-// Filtrar películas por género
+// Esta funcioon la creo para filtrar películas por género
 const peliculasAccion = peliculas.filter((pelicula) =>
   pelicula.genre_ids.includes(28)
 );
@@ -44,7 +44,7 @@ const peliculasAventura = peliculas.filter((pelicula) =>
   pelicula.genre_ids.includes(12)
 );
 
-// Agregar películas filtradas a los contenedores correspondientes
+// asi muestro las películas filtradas en los contenedores correspondientes
 agregarPeliculasAContenedor(peliculasAccion, generoAccion);
 agregarPeliculasAContenedor(peliculasThriller, generoThriller);
 agregarPeliculasAContenedor(peliculasAventura, generoAventura);
